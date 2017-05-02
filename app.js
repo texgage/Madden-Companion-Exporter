ff03c091b8db72e9b870fa2b08214fd96ad861c5
texgage <texgage@gmail.com>
2017-05-02 11:46:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-const serviceAccount = require("madden-bowl-firebase-adminsdk-yo5bt-09cbbc4bb9.json");
+const serviceAccount = require("./madden-bowl-firebase-adminsdk-yo5bt-150ab78f65.json");
 
 // TODO: Enter your database url from firebase
 admin.initializeApp({
